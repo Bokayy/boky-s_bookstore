27adff34ddc76a8c4d808a3dbcd43df76d596a8b
Bokayy X <blabsy8@gmail.com>
2023-10-27 01:25:25 +0200

    changed port to something that hopefully doesn't get taken

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ from "./database.js";
 const app = express();
 
 app.use(express.json());
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:2337');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
