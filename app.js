bcb636c713686261bbece3487dc7def1cf749a7e
Boris Milojević <45460628+Bokayy@users.noreply.github.com>
2024-11-30 18:31:45 +0100

    changed port to 2052 to use http instead of https

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.post("/insert", async (req,res) => {
 });
 
 //init server, entrypoint
-app.listen(2053, '0.0.0.0', ()=> {
+app.listen(2052, '0.0.0.0', ()=> {
     console.log('Server is running on port 2053');
 })
 
